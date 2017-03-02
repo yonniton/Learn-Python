668ed64b62aebbabbbe4533abf21f69005a02508
Spuzzy <spuzzy.buzzy@gmail.com>
2017-03-02 21:03:34 +0800

    Learn Python the Hard Way - Exercise 1

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# print "Hello World !"
+# print "Hello again"
+# print "I like typing like this."
+# print "This is fun."
+# print 'Yay! Printing.'
+# print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "I didn't know a hash is also known as an 'octothrophe'."
