457d9545998689cf9b1e60a939976ebcf0e31480
Spuzzy <spuzzy.buzzy@gmail.com>
2017-03-03 11:01:41 +0800

    Learn Python the Hard Way - Exercise 5

--- ex5.py
+++ ex5.py
@@ -1,19 +1,21 @@
 name = 'Zed A.Shaw'
 age = 35 # not a lie
-height = 74 # inches
-weight = 180 # lbs
+height_inches = 74 # inches
+weight_pounds = 180 # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
-cm = inches * 2.54
-kg = pounds * 0.45359237
+height_cm = height_inches * 2.54
+weight_kg = weight_pounds * 0.45359237
 
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height
-print "He's %d pounds heavy." % weight
+print "He's %d inches tall." % height_inches
+print "He's %d cm tall." $ height_cm
+print "He's %d pounds heavy." % weight_pounds
+print "He's %d kg heavy." % weight_kg
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
+print "If I add %d, %d, and %d I get %d." % (age, height_inches, weight_pounds, age + height_inches + weight_pounds)
