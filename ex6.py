28be164530feb5d63006d5c1b171e8c8c19b7388
Jonathan Teo <jtmh@hotmail.com>
2017-03-06 10:23:18 +0800

    ex6 : Strings and Text
    ### Study Drill
    2. identify strings-within-strings

--- ex6.py
+++ ex6.py
@@ -1,16 +1,16 @@
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
-y = "Those who know %s and those who %s." % (binary, do_not)
+y = "Those who know %s and those who %s." % (binary, do_not)  # 2 `string` arguments
 
 print x
 print y
 
-print "I said: %r." % x
-print "I also said: '%s'." % y
+print "I said: %r." % x                                       # 1 `raw` argument
+print "I also said: '%s'." % y                                # 1 `raw` argument
 
 hilarous = False
-joke_evaluation = "Isn't the joke so funny?! %r"
+joke_evaluation = "Isn't the joke so funny?! %r"              # 1 `raw` argument
 
 print joke_evaluation % hilarous
 
