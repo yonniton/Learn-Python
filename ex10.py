19c7454f4babe6b7554eb72f4b8de67306193bd6
Jonathan Teo <jonathanteo@stee.stengg.com>
2017-03-06 15:06:59 +0800

    ex10 : What Was That?
    ### Study Drill
    2. use triple single-quotes
    4. use format-strings and escape-sequences; `%r` vs `%s`

--- ex10.py
+++ ex10.py
@@ -1,15 +1,19 @@
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
+tabby_cat = "%rI'm tabbed in." % '\t'
+persian_cat = "I'm split%son a line." % '\n'
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+'''
 
 print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+# while True:
+#     for i in [",", "-", "|", "\\", "|"]:
+#         print "%s\r" % i,
