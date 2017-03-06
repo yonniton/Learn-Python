d296071ca1400c9143e8aff1705294da07488c15
Jonathan Teo <jonathanteo@stee.stengg.com>
2017-03-06 16:00:53 +0800

    ex13 : Parameters, Unpacking, Variables
    ### Study Drills
    3. capture with `raw_input`; experiment with `str.split`

--- ex13.py
+++ ex13.py
@@ -6,3 +6,6 @@ print "The script is called:", script
 print "The first variable is:", first
 print "The second variable is:", second
 print "The third variable is:", third
+
+str_args = raw_input("Whitespace-delimited args? ")
+print "The remaining variables are: %s" % str_args.split()
