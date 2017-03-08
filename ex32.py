d32fa37ae37d00ca27800ade3e79287336148ffb
Jonathan Teo <jtmh@hotmail.com>
2017-03-08 15:32:38 +0800

    ex32 : Loops and Lists
    ### Study Drills
    2. directly assign to `elements` using function `range`

--- ex32.py
+++ ex32.py
@@ -15,14 +15,8 @@ for fruit in fruits:
 for i in change:
     print "I got %r" % i
 
-# we can also build lists, first start with an empty one
-elements = []
-
-# then use the range function to do 0 to 5 counts
-for i in range(0, 6):
-    print "Adding %d to the list." % i
-    # append is a function that lists understand
-    elements.append(i)
+# we can also build lists, using the range function
+elements = range(0, 6)
 
 # now we can print them out too
 for i in elements:
