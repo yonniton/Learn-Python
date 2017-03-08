74ebc845e6459661e2d0625d7b84f6772c3749db
Jonathan Teo <jtmh@hotmail.com>
2017-03-08 23:24:52 +0800

    ex33 : While Loops
    ### Study Drills
    1. extracted function
    2. parameterized for different upper-bounds
    3. parameterized for different increment-steps
    4. replace `while` with `for-range`

--- ex33.py
+++ ex33.py
@@ -1,16 +1,24 @@
-i = 0
+from sys import argv
+
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+def append_inc_nums_till(top, step=1):
+    for i in range(0, top, step):
+        print "At the top i is %d" % i
+        numbers.append(i)
 
+        i += step
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+script, upper_limit, inc = argv
+
+append_inc_nums_till(
+    int(upper_limit),
+    int(inc)
+)
 
 print "The numbers: "
-
 for num in numbers:
     print num
